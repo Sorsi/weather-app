cd662e553a8db4990e8318b3d15462429facec75
Orsi Suto <orsolya.suto.so@gmail.com>
2023-05-27 00:01:50 +0200

    Updates

---
+++ precache-manifest.395ba4ac8454f9f319b8e67f0230f216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "84ae295c4bcd5ae9ab1c",
+    "url": "/weather-app/static/css/main.7dc9cca4.chunk.css"
+  },
+  {
+    "revision": "84ae295c4bcd5ae9ab1c",
+    "url": "/weather-app/static/js/main.84ae295c.chunk.js"
+  },
+  {
+    "revision": "c28db6f788cdeff29fb6",
+    "url": "/weather-app/static/css/1.dc4ed362.chunk.css"
+  },
+  {
+    "revision": "c28db6f788cdeff29fb6",
+    "url": "/weather-app/static/js/1.c28db6f7.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "c0f9638df716ab5a0ab3cddac105513b",
+    "url": "/weather-app/static/media/bg42.c0f9638d.jpg"
+  },
+  {
+    "revision": "4946bbd23eda52becaea9dbbea4e6534",
+    "url": "/weather-app/index.html"
+  }
+];
